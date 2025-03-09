1e0aea4bdd0e8ae844060bdad3bb5fb01bf4c7f0
Hien <nguy1109@algonquinlive.com>
2025-03-09 05:05:53 -0400

    commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const animals = [
+    { char: "🐉", code: "128009", name: "Dragon" },
+    { char: "🐊", code: "128010", name: "Crocodile" },
+    { char: "🐋", code: "128011", name: "Whale" },
+    { char: "🐌", code: "128012", name: "Snail" },
+    { char: "🐍", code: "128013", name: "Snake" },
+    { char: "🐎", code: "128014", name: "Horse" },
+    { char: "🐏", code: "128015", name: "Ram" },
+    { char: "🐐", code: "128016", name: "Goat" },
+    { char: "🐑", code: "128017", name: "Sheep" },
+    { char: "🐒", code: "128018", name: "Monkey" },
+    { char: "🐓", code: "128019", name: "Rooster" }
+];
+
+const gallery = document.getElementById("emoji-gallery");
+
+animals.forEach(animal => {
+    const emojiDiv = document.createElement("div");
+    emojiDiv.classList.add("emoji-item");
+
+    emojiDiv.innerHTML = `
+        <div class="emoji">${animal.char}</div>
+        <div class="unicode">Code: &#${animal.code};</div>
+        <div class="name">${animal.name}</div>
+    `;
+
+    gallery.appendChild(emojiDiv);
+});
